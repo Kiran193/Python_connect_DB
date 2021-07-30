2a3c8b0ab9143cd20136b5e5866f4df2d790bc3b
Kiran Thakur <31925562+Kiran193@users.noreply.github.com>
2021-07-30 15:58:14 +0530

    Removed credential details

--- config_win.py
+++ config_win.py
@@ -21,12 +21,6 @@ sct_config = dict(
     track_detection_iou_thresh=0.5
 )
 
-#Model files. TODO: Change from static to dynamic paths.
-person_detect_model = "counter\\models\\person-detection-retail-0013.xml"
-person_reid_model = "counter\\models\\person-reidentification-retail-0200.xml"
-face_det_model="counter\\models\\face-detection-retail-0005.xml"
-face_weights="counter\\models\\face-detection-retail-0005.bin"
-mask_det_model="counter\\face_detector_mask\\mask_detector.model"
 
 threshold_person_detection = 0.80
 faceconfidence_mask = 0.90
@@ -34,12 +28,8 @@ device = "CPU"
 cpu_extension = None # (MKLDNN (CPU)-targeted custom layers. Absolute path to a shared library with the kernels impl.', type=str)
 enter_to_db = True
 save_video=True
-output_video_path="D:\\Work\\counter-electron-app\\"
 
 show_video=True
-demo_video_feed=['D:\\Work\\counter-electron-app\\normal_entry.mp4']
-# demo_video_feed=['rtsp://admin:kayathebird!@192.168.1.64:554/H.264']
-
 
 
 #store details

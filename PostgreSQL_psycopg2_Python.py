1ccf9cef03de48b66940d7235e8bdfbefa80d6ca
Kiran Thakur <31925562+Kiran193@users.noreply.github.com>
2021-07-30 15:46:23 +0530

    Updated credential with dummy value

--- PostgreSQL_psycopg2_Python.py
+++ PostgreSQL_psycopg2_Python.py
@@ -14,7 +14,7 @@ with open('config.json','r') as f:
 	config = json.load(f)
 
 app.config["DEBUG"] = True
-app.config['SECRET_KEY'] = 'BravisaTempleTreeEncryptedToken'
+app.config['SECRET_KEY'] = 'SECRETKEY'
 
 
 DBhost = config['DBLocal']['host']
@@ -40,12 +40,12 @@ def db_connect():
         app.config["DEBUG"] = True
         return conn
     else:
-        conn = psycopg2.connect(database="BravisaDB", user="sid", password="kayasid2018", host="/cloudsql/bravisa-temple-tree:asia-south1:btt-db", port="5432")
+        conn = psycopg2.connect(database="DBNAME", user="USERNAME", password="PASSWORD", host="HOSTNAMEIP_or_NAME", port="PORT")
         return conn
 
 def db_connect_local():
 
-    conn = psycopg2.connect(database="db_Test", user="postgres", password="postgres")
+    conn = psycopg2.connect(database="DBNAME", user="USERNAME", password="PASSWORD")
     app.config["DEBUG"] = True
     print(conn)
     return conn
